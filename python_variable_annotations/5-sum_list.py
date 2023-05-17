406311d278ac34fc56163ef36eadc76996b030a8
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2023-05-17 16:56:42 +0100

    sumlist

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Input_list"""
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """Returns  sum of all elements in list"""
+    return sum(input_list)
