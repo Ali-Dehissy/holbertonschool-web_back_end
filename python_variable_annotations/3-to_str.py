6841ac482341581d41532379429c86ea6d2ee877
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2023-05-17 15:48:12 +0100

    string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Takes a float n as argument and returns the string representation
+of the float"""
+
+
+def to_str(n: float) -> str:
+    """Returning n as a string"""
+    return str(n)
