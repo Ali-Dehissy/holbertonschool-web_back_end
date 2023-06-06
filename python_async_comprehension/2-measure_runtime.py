e1fd9bc06a9b913db3250e434141c0637f32e64b
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2023-06-06 13:25:21 +0100

    Create 2-measure_runtime.py

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Async measure time"""
+import time
+import asyncio
+from typing import List
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Executing async comprehension"""
+    time1 = time.time()
+    await asyncio.gather(async_comprehension())
+    time2 = time.time()
+    total_t = time2 - time1
+    return total_t
