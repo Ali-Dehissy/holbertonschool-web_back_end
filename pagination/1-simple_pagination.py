cec74769830b70145a7ef6e421c17e50e9fe9c00
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2023-06-22 12:06:31 +0100

    pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Pagination"""
+import csv
+import math
+from typing import List, Tuple
+
+
+class Server:
+    """Class server"""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Dataset func"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        pass
+
+    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
+        """Returning a tuple"""
+        end: int = page * page_size
+        start: int = 0
+        for _ in range(page - 1):
+            start += page_size
+        return (start, end)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get page pagination"""
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        dataset = self.dataset()
+        start, end = self.index_range(page, page_size)
+        if end > len(dataset):
+            return []
+        return [list(dataset[row]) for row in range(start, end)]
