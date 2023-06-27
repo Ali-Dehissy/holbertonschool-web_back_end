e88231cc000701bb08f716211ceb4c76404d4f27
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2023-06-27 14:04:20 +0100

    update id attributes

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 -- SQL script creating a table
 CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY,
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
   email VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(255),
   country ENUM('US', 'CO', 'TN') NOT NULL
