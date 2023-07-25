443fbc1da86df4a07b23931c7f377221a79e6374
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2023-07-25 21:11:14 +0100

    gorcerieslist

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+function groceriesList() {
+  const grocerieslist = new Map();
+
+  groceries.set('Apples', 10);
+  groceries.set('Tomatoes', 10);
+  groceries.set('Pasta', 1);
+  groceries.set('Rice', 1);
+  groceries.set('Banana', 5);
+
+  return grocerieslist;
+}
+
+export default groceriesList;
