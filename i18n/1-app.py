2bcb0489e7727134fe35acf5e42cb87424c78523
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2023-12-11 13:10:53 +0100

    app  flask

---
+++ i18n/1-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Flask application"""
+
+from flask import Flask, request, render_template
+from flask_babel import Babel
+from os import getenv
+
+app = Flask(__name__, static_url_path='')
+babel = Babel(app)
+
+
+class Config(object):
+    """Babel configuration"""
+    LANGUAGES = ['en', 'fr']
+    # these are the inherent defaults just btw
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object('1-app.Config')
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """Returns index.html"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
