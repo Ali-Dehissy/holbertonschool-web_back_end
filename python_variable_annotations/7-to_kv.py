29f7091535d725fe08a1f2d92eb98ff8b8127162
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2024-05-27 15:52:44 +0100

    comment update

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-"""Complex types"""  
+"""
+Complex types - String and int
+"""
 from typing import List, Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Returns Tuple"""
+    """
+    Return Tuple
+    """
     return (k, v**2)
