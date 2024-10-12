e544b899ddb800719b1411782381debdc9b4bf51
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2024-10-12 22:16:33 +0100

    update

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-"""Python function that inserts new document"""
+"""Insert a document"""
+
 
 def insert_school(mongo_collection, **kwargs):
-    """Mongo function"""
-    document_id = mongo_collection.insert(kwargs)
-    return document_id
+    """ Function that inserts a new document
+    in a collection based on kwargs"""
+    result = mongo_collection.insert_one(kwargs)
+    return result and result.inserted_id
