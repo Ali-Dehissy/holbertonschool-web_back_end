4a749ba03c21b9c6ea13b07ae2bd139ca98988f7
Ali-Dehissy <ali1998dehissy@gmail.com>
2024-10-25 22:49:30 +0200

    Unittests

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+class TestGithubOrgClient(unittest.TestCase):
+    @parameterized.expand([
+        ("google",),
+        ("abc",)
+    ])
+    @patch('client.get_json', return_value={"org_name": "dummy_org"})
+    def test_org(self, org_name, mock_get_json):
+        client = GithubOrgClient(org_name)
+        result = client.org()
+
+        mock_get_json.assert_called_once_with(GithubOrgClient.ORG_URL.format(org=org_name))
+        self.assertEqual(result, {"org_name": "dummy_org"})
