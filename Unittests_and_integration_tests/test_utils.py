5894c75d41edaa9dd32defb07cad17cae5b36942
Ali-Dehissy <ali1998dehissy@gmail.com>
2024-10-25 23:26:47 +0200

    remaining files

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,18 +1,80 @@
 #!/usr/bin/env python3
+"""Test"""
 import unittest
-from unittest.mock import patch
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
-from client import GithubOrgClient
+from typing import Mapping, Sequence
+from unittest import mock
+from unittest.mock import patch
 
-class TestGithubOrgClient(unittest.TestCase):
+
+class TestAccessNestedMap(unittest.TestCase):
+    """test Accessnestedmap"""
     @parameterized.expand([
-        ("google",),
-        ("abc",)
+        ({"a": 1}, ["a"], 1),
+        ({"a": {"b": 2}}, ["a"], {"b": 2}),
+        ({"a": {"b": 2}}, ["a", "b"], 2),
     ])
-    @patch('client.get_json', return_value={"org_name": "dummy_org"})
-    def test_org(self, org_name, mock_get_json):
-        client = GithubOrgClient(org_name)
-        result = client.org()
+    def test_access_nested_map(self, nested_map:
+                               Mapping, path: Sequence,
+                               expected):
+        """Test Method that access_nested_map and
+        returns what it is supposed to"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
-        mock_get_json.assert_called_once_with(GithubOrgClient.ORG_URL.format(org=org_name))
-        self.assertEqual(result, {"org_name": "dummy_org"})
+    @parameterized.expand([
+        ({}, ["a"], KeyError),
+        ({"a": 1}, ["a", "b"], KeyError)
+    ])
+    def test_access_nested_map_exception(self, nested_map:
+                                         Mapping, path: Sequence,
+                                         expected):
+        """test that a KeyError is raised"""
+        with self.assertRaises(KeyError) as raises:
+            access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Test get_json"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url, payload):
+        """Test method if utils.get_json
+        returns the expected result"""
+        response = mock.Mock()
+        response.json.return_value = payload
+
+        with mock.patch('requests.get', return_value=response):
+            request = get_json(url)
+            self.assertEqual(request, payload)
+            response.json.assert_called_once()
+
+
+class TestMemoize(unittest.TestCase):
+    """TestMemoize"""
+
+    def test_memoize(self):
+        """TestMemoize"""
+        class TestClass:
+            """Test Class"""
+
+            def a_method(self):
+                """Method"""
+                return 42
+
+            @memoize
+            def a_property(self):
+                """Returns Memoized property"""
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as patched:
+            test_class = TestClass()
+            returned = test_class.a_property
+            self.assertEqual(returned, 42)
+            patched.assert_called_once()
+
+
+if __name__ == '__main__':
+    unittest.main()
